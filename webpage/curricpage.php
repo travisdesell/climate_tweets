f17d6c04bf8d389f296f95d874121e079ac71ed1
lindseywingate <lindseymwingate@gmail.com>
2016-04-06 14:50:37 -0500

    added a bunch of new buttons, pdf files, etc.

--- webpage/curricpage.php
+++ webpage/curricpage.php
@@ -22,38 +22,101 @@ print_navbar("Projects: Climate Tweets", "Climate Tweets", "..");
 echo "
 <div class='container'>
     <div class='row'>
-	<div class='col-sm-8'>
-        <h1>Climate Tweets - Lesson Plans</h1>
+		<div class='col-sm-12'>
+			<div class='well'>
+        		<h1>Climate Tweets - Lesson Plans</h1>
+			</div><!--well-->
+		</div><!--col-->
+	</div><!--row-->
+	<div class='row'>
+		<div class='col-sm-6'>
         	<div class='well'>
+				<h4>K-2 Lesson Plan</h4>
+					<p>Develop understanding of the difference between climate and weather.</p>
 				<a href='docs/K-2.docx'>
-				<button type='button' class='btn btn-default'>
-					K-2 Lesson Plan
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc
+				</button>
+				</a>
+				<a href='docs/K-2.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
 				</button>
 				</a>
 
+			</div><!--div-->
+		</div><!--col-->
+		<div class='col-sm-6'>
+			<div class='well'>
+				<h4>3-5 Lesson Plan</h4>
+					<p>Develop an understanding of why climate change is considered a problem and encourage students to take action.</p>	
 				<a href='docs/3-5.docx'>
-				<button type='button' class='btn btn-default'>
-					3-5 Lesson Plan
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc
+				</button>
+				</a>
+				<a href='docs/3-5.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
 				</button>
 				</a>
 
+			</div><!--well-->		
+		</div><!--col-->
+	</div><!--row-->
+	<div class='row'>
+		<div class='col-sm-6'>
+			<div class='well'>
+				<h4>6-8 Lesson Plan</h4>
+					<p>Develop data analysis skills and critically evaluate opinion vs. fact.</p>
 				<a href='docs/6-8.docx'>
-				<button type='button' class='btn btn-default'>
-					6-8 Lesson Plan
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc 
+				</button>
+				</a>	
+				<a href='docs/6-8.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
 				</button>
 				</a>
 
+			</div><!--well-->
+		</div><!--col-->
+
+		<div class='col-sm-6'>
+			<div class='well'>
+				<h4>9-10 Lesson Plan</h4>
+					<p>Critically interpret public opinion of climate change and provide an accurate analysis for review by researchers.</p>
 				<a href='docs/9-10.docx'>
-				<button type='button' class='btn btn-default'>
-					9-10 Lesson Plan
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc 
+				</button>
+				</a>
+				<a href='docs/9-10.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
 				</button>
 				</a>
 
-				<a href='11-12.doc'>
-				<button type='button' class='btn btn-default'>
-					11-12 Lesson Plan
+			</div><!--well-->
+		</div><!--col-->
+	</div><!--row-->	
+	<div class='row'>
+		<div class='col-sm-6'>
+			<div class='well'>
+				<h4>11-12 Lesson Plan</h4>
+					<p>Develop data analysis skills and critically evaluate opinion vs. fact.</p>
+				<a href='docs/11-12.docx'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc
 				</button>
 				</a>
+				<a href='docs/11-12.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF
+				</button>
+				</a>
+
 			</div><!--well-->
 		</div><!--col-->
     </div><!--row-->
