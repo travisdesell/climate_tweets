2aa30b94af05de7c628ee45ffa3297b25a44d46c
lindseywingate <lindseymwingate@gmail.com>
2016-04-12 01:26:40 -0500

    finally got some text from the database. ditching template

--- webpage/K2.php
+++ webpage/K2.php
@@ -11,33 +11,41 @@ require_once($cwd[__FILE__] . "/../../citizen_science_grid/my_query.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/user.php");
 require_once($cwd[__FILE__] . "/get_languages.php");
 require_once($cwd[__FILE__] . "/get_next_tweet.php");
-/*require_once($cwd[__FILE__] . "/../mustache.php/src/Mustache/Autoloader.php";
-Mustache_Autoloader::register();
-*/
-$user = csg_get_user();
-$user_id = $user['id'];
+//$user = csg_get_user();
+//$user_id = $user['id'];
 
 $css_header = "<link rel='stylesheet' type = 'text/css' href = 'css/style.css' />";
 
 print_header("Tweet Selection", "$css_header <script type='text/javascript' src='js/climate_tweets.js'></script><script type='text/javascript' src='js/discuss.js'></script>", "dna");
 print_navbar("Projects: Climate Tweets", "Climate Tweets", "..");
 
-$results = query_boinc_db("SELECT text FROM climate_tweets where prof = 0 LIMIT 30");
-	$row=$results->fetch_assoc();
+$results = query_boinc_db("SELECT text FROM climate_tweets where prof = 0 LIMIT 1");
+while($row=mysqli_fetch_array($results)) {
+	echo $row['text'];
+}
 
-$testing=array(
-	'text'=>'testing'
-);
+echo "
+<div class='container'>
+	<div class= 'row'>
+		<div class='col'>
+			<div class='well'>
+				<table class='table table-bordered'>
+					<h3>
+					Please select the desired tweets
+					</h3>
+					<tbody>
+						<tr>
+							<td><div class='radio'><label><input type='radio' class='attitude-radio'></label></div></td>
+							<td>test test </td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
 
-//class testing {
-//	public $text = "blah";
-//}
-
-$tweet_selection = file_get_contents($cwd[__FILE__] . "/templates/tweet_selection.html");
-
-$m = new Mustache_Engine;
-//$testing = new testing;
-echo $m->render($tweet_selection, $testing);
+";
 
 print_footer('<strong>Travis Desell and the Climate Tweets Team</strong>', '<strong>Aaron Bergstrom, Travis Desell, Lindsey Wingate, and Andrei Kirilenko</strong>');
 echo "</body></html>";
