42c8f681907b96bb0c04b8ee9422dd8a9c54ae19
lindseywingate <lindseymwingate@gmail.com>
2016-04-13 21:27:54 -0500

    commiting

--- webpage/language_cleanup.py
+++ webpage/language_cleanup.py
@@ -7,27 +7,25 @@ from mysql.connector import errorcode
 import langdetect
 from langdetect import detect
 
-#import MySQLdb.cursors
+import MySQLdb.cursors
 
-
-try:
-    database = mysql.connector.connect(host = "localhost", user="tdesell", passwd= "TDBoinc12", db="csg", user_pure = True) # cursorclass = MySQLdb.cursors.DictCursor)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_EERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-    else:
-        database.close()
-
+#try
+database = mysql.connector.connect(host = "127.0.0.1", user="tdesell", passwd= "TDBoinc12", db="csg") # cursorclass = MySQLdb.cursors.DictCursor)
+#except mysql.connector.Error as err:
+   # if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    #    print("Something is wrong with your user name or password")
+    #if err.errno == errorcode.ER_BAD_DB_EERROR:
+     #   print("Database does not exist")
+      #  print(err)
+  #  else:
+   #     database.close()
+#	print("Unsuccessful Connection")
 
 cleaner = database.cursor() #can specify cursorclass parameter
 
 cleaner.execute("SELECT * FROM climate_tweets")
 #tweets = cleaner.fetchall()#select text, lang detect
-**tweets = cleaner.fetchmany()
+tweets = cleaner.fetchmany()
 #fetchmany(size=num!) selects batch of rows and returns as list of tuples!! returns empty list once rows are all gone
 
 for tweet in tweets:
