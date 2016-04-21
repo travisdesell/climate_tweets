1418c9df0cd904cfe4c218a10a218b8f03778955
lindseywingate <lindseymwingate@gmail.com>
2016-04-21 10:24:52 -0500

    working to get js to write to the downloadable file

--- webpage/js/K2.js
+++ webpage/js/K2.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
 	var selected = [];
 
 	$('.selection_box').click(function() {
-		if($(this).prop('checked')) 
+		if($(this).prop('checked')) { 
 			console.log($(this).attr('id') + ' is checked');
-		else 
+			selected.push($(this).attr('id'));
+		}
+		else {
 			console.log($(this).attr('id') + ' is not checked');
-	
+			for(var y=0; y<selected.length; y++) {
+				if(selected[y] == $(this).attr('id')) {
+					selected.splice(y, 1);
+				}
+			}
+		}
 	});
+
+/*	$('.download_button').click(function() {
+			
+	});
+*/
 });
