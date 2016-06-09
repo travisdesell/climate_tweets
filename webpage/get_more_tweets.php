6e4447b6008bf785f9c41d9f0caae3c1b14ba7e9
lindseywingate <lindseymwingate@gmail.com>
2016-06-09 18:45:31 -0500

    finally got tweets dynamically adding to k2 page :) now just need to get checkboxes adding to array to add to text document..

--- webpage/get_more_tweets.php
+++ webpage/get_more_tweets.php
@@ -6,9 +6,20 @@ $cwd[__FILE__] = dirname($cwd[__FILE__]);
 
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/my_query.php");
 
-$results = query_boinc_db("SELECT text FROM climate_tweets where lang='en' and prof=0 order by rand() LIMIT 10"); 
+$results = query_boinc_db("SELECT text FROM climate_tweets where lang='en' and prof=0 order by rand() LIMIT 20"); 
 
-$row = $results->fetch_assoc();
+$pass_me = array();
 
-echo json_encode($row);
+for($x=0;$x<20; $x++) {
+	$row = $results->fetch_assoc();
+	echo $row["text"];
+	echo "****";
+	array_push($pass_me, $row["text"]);
+}
+
+//print_r($pass_me);
+
+//echo json_encode($pass_me);
+//
+//echo json_encode($row["text"]);
 ?>
--- webpage/js/K2.js
+++ webpage/js/K2.js
@@ -1,6 +1,36 @@
 $(document).ready(function() {
 	var selected = [];
 
+	$('#render_doc').click(function() {
+		for(var x=0; x<selected.length; x++) {
+			selected[x] = selected[x] + "\r\n\r\n";
+		}
+		var data = new Blob(selected,{type: "text/plain;charset=utf-8"});
+		saveAs(data, 'tweets.txt');
+	});//click function
+
+//when this button is pushed, an ajax call is made to get_more_tweets.php, which queries the database for 20 more tweets. 
+	$('#more_tweets').click(function() {
+		//ajax call for php script for new html, then append
+		$.ajax({
+			type : 'POST',
+			url : './get_more_tweets.php',
+			data : JSON,
+		
+			success : function(data) {
+				//console.log("More tweets were received!");
+				console.log("php returned '" + JSON.stringify(data) + "'");
+				//data = data.slice(9, -2);//cleans text for use in id and visual on site
+				//console.log(data);
+				var new_tweets = data.split("****");
+				for(var x=0; x<20; x++) 
+				{
+				$("#mytable").append("<tr><td><center><div class='checkbox'><label><input type='checkbox' class='selection box'></label></div></center></td><td>"+new_tweets[x]+"</td></tr>");	
+				}
+			}
+		});
+	});
+
 	$('.selection_box').click(function() {
 		if($(this).prop('checked')) { 
 			console.log($(this).attr('id') + ' is checked');
@@ -14,32 +44,5 @@ $(document).ready(function() {
 				}
 			}
 		}
-	});
-
-	$('#render_doc').click(function() {
-		for(var x=0; x<selected.length; x++) {
-			selected[x] = selected[x] + "\r\n\r\n";
-		}
-		var data = new Blob(selected,{type: "text/plain;charset=utf-8"});
-		saveAs(data, 'tweets.txt');
-	});//click function
-
-	$('#more_tweets').click(function() {
-		//ajax call for php script for new html, then append
-		$.ajax({
-			type : 'POST',
-			url : './get_more_tweets.php',
-			data : JSON,
-		
-			success : function(data) {
-				console.log("More tweets were received!");
-				console.log("php returned '" + data + "'");
-				//for(var x=0; x<10; x++) {
-					$("#mytable").append("<tr><td><center><div class='checkbox'><label><input type='checkbox' class='selection box'></label></div></center></td><td>new</td></tr>");	
-					//$("td").append("this is da text"); //appends that text 10 times in each checkbox and after each tweets text
-					//$("td").append("<td><div class='checkbox'><label><input type='checkbox' class='selection_box'></label></div></td><td>data[x]</td>");//appends a ton of disorganized checkboxes.		
-				//}
-			}
-		});
 	});
 });
