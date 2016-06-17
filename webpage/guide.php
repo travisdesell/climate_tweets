57eb8db0055cba0edbae82e607a821fefc92caf7
lindseywingate <lindseymwingate@gmail.com>
2016-06-17 14:13:09 -0500

    still updating changes on guide page

--- webpage/guide.php
+++ webpage/guide.php
@@ -16,7 +16,7 @@ echo "
 <div class='container'>
 	<div class='row'>
 		<div class='col-sm-12'>
-			<center><h1>Teacher's Guide</h1></center>				
+			<center><h1>Educator's Guide</h1></center>				
 		</div><!--col-->
 	</div><!--row-->
 	<div class='row'>
@@ -41,24 +41,77 @@ echo "
 				Select Tweets
 				</button>
 				</a>
-
+				<br><br>
+				<p>Additional Materials: \nPut books here</p>
 			</div><!--well-->
 		</div><!--col-->
 		<div class='col-sm-6'>
 			<div class='well'>
-				3-5
+				<h4>Grades 3-5</h4>
+					<p>Help the students develop an understanding of why climate change is considered a problem and encourage students to take action.</p>	
+				<a href='docs/3-5.docx'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc
+				</button>
+				</a>
+				<a href='docs/3-5.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
+				</button>
+				</a>
+				<br><br>
+				<p>Additional Materials: Put books here</p>
 			</div><!--well-->
 		</div><!--col-->
 	</div><!--row-->
 	<div class='row'>
 		<div class='col-sm-6'>
 			<div class='well'>
-				6-8
+				<h4>Grades 6-8</h4>
+					<p>Help the students develop data analysis skills and critically evaluate opinion vs. fact.</p>
+				<a href='docs/6-8.docx'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc 
+				</button>
+				</a>	
+				<a href='docs/6-8.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
+				</button>
+				</a>
+				<br><br>
+				<p>Create a list of tweets the students will analyze.</p>
+				<a href='http://csgrid.org/csg/climate_lwingate/K2.php'>
+				<button type='button' class='btn btn-default btn-sm'>
+				Select Tweets
+				</button>
+				</a>
+				<br><br>
+				<p>Additional Materials: \nPut books here</p>
 			</div><!--well-->
 		</div><!--col-->
 		<div class='col-sm-6'>
 			<div class='well'>
-				9-12
+				<h4>Grades 9 and 10</h4>
+					<p>Critically interpret public opinion of climate change and provide an accurate analysis for review by researchers.</p>
+				<a href='docs/9-10.docx'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download Word Doc 
+				</button>
+				</a>
+				<a href='docs/9-10.pdf'>
+				<button type='button' class='btn btn-default btn-sm'>
+					Download PDF 
+				</button>
+				</a>
+
+			</div><!--well-->
+		</div><!--col-->
+	</div><!--row-->
+	<div class='row'>
+		<div class='col-sm-6'>
+			<div class='well'>
+				<h4>Grades 11 and 12</h4>
 			</div><!--well-->
 		</div><!--col-->
 	</div><!--row-->
