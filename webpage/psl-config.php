c77b441f6219fb4e84e4b6969be717b027a419bd
lindseywingate <lindseymwingate@gmail.com>
2016-06-24 14:58:17 -0500

    database config file

---
+++ webpage/psl-config.php
@@ -0,0 +1,12 @@
+<?PHP
+
+define("HOST", "localhost");
+define("USER", "lwingate");
+define("PASSWORD", "climate password");
+define("DATABASE", "climate_tweets");
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);
+?>
